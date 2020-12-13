8fb113deb0d3aa406fa74a0cb954e23c1fc83f8c
Mahfuz <75206652+mahfuzbd71@users.noreply.github.com>
2020-12-13 10:11:32 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+
+
+        autoplay:true,
+        autoplaytimeout:1000,
+        margin:10,
+        loop:true,
+        autoplayhoverpause:true,
+
+              
+    });
+
+  });
